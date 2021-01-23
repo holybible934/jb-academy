dbc11b7b8b9a5bcf98282b674eecffb97447e0b0
CHChang <holybible934@gmail.com>
2021-01-23 15:13:20 +0800

    Handle test 18 - source base is 1

--- NumeralSystemConverter.java
+++ NumeralSystemConverter.java
@@ -9,36 +9,47 @@ public class Main {
         scanner.nextLine();
         StringBuilder output = new StringBuilder();
         String buf = scanner.nextLine();
-        if (buf.contains(".")) {
-            String[] input = buf.split("\\.");
-            int intPart = Integer.parseInt(input[0], sourceBase);
-            char[] decimals = input[1].toCharArray();
-            double fracPart = 0.0;
-            for (int i = 0; i < decimals.length; i++) {
-                int digit = Character.getNumericValue(decimals[i]);
-                fracPart += digit / Math.pow(sourceBase, i + 1);
-            }
+        if (sourceBase == 1) {
+            int intPart = buf.length();
             int newBase = scanner.nextInt();
-            output = new StringBuilder(Integer.toString(intPart, newBase) + '.');
-            for (int i = 0; i < 5; i++) {
-                String temp = String.valueOf(fracPart * newBase);
-                int beforeDecimals = (int) Double.parseDouble(temp);
-                output.append(Character.forDigit(beforeDecimals, newBase));
-                int indexOfDecimal = temp.indexOf(".");
-                fracPart = Double.parseDouble("0" + temp.substring(indexOfDecimal));
-            }
-        } else {
-            int intPart = Integer.parseInt(buf, sourceBase);
-            int newBase = scanner.nextInt();
-            if (newBase > 1) {
-                output = new StringBuilder((Integer.toString(intPart, newBase)));
-            }
-            else {
-                while(intPart-- > 0) {
-                    output.append(1);
+            output = getOutput(output, intPart, newBase);
+        }
+        else {
+            if (buf.contains(".")) {
+                String[] input = buf.split("\\.");
+                int intPart = Integer.parseInt(input[0], sourceBase);
+                char[] decimals = input[1].toCharArray();
+                double fracPart = 0.0;
+                for (int i = 0; i < decimals.length; i++) {
+                    int digit = Character.getNumericValue(decimals[i]);
+                    fracPart += digit / Math.pow(sourceBase, i + 1);
                 }
+                int newBase = scanner.nextInt();
+                output = new StringBuilder(Integer.toString(intPart, newBase) + '.');
+                for (int i = 0; i < 5; i++) {
+                    String temp = String.valueOf(fracPart * newBase);
+                    int beforeDecimals = (int) Double.parseDouble(temp);
+                    output.append(Character.forDigit(beforeDecimals, newBase));
+                    int indexOfDecimal = temp.indexOf(".");
+                    fracPart = Double.parseDouble("0" + temp.substring(indexOfDecimal));
+                }
+            } else {
+                int intPart = Integer.parseInt(buf, sourceBase);
+                int newBase = scanner.nextInt();
+                output = getOutput(output, intPart, newBase);
             }
         }
         System.out.println(output);
+    }
+
+    private static StringBuilder getOutput(StringBuilder output, int intPart, int newBase) {
+        if (newBase > 1) {
+            output = new StringBuilder((Integer.toString(intPart, newBase)));
+        } else {
+            while (intPart-- > 0) {
+                output.append(1);
+            }
+        }
+        return output;
     }
 }
