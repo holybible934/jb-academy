6484b0f9b785848506810a3a6ee3d831ccc9281a
CHChang <holybible934@gmail.com>
2021-01-23 15:35:19 +0800

    Add error handling

--- NumeralSystemConverter.java
+++ NumeralSystemConverter.java
@@ -5,7 +5,14 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        int sourceBase = scanner.nextInt();
+        int sourceBase;
+        try {
+            sourceBase = scanner.nextInt();
+        }
+        catch (Exception ex) {
+            System.out.println("error source base");
+            return;
+        }
         scanner.nextLine();
         StringBuilder output = new StringBuilder();
         String buf = scanner.nextLine();
@@ -17,6 +24,20 @@ public class Main {
         else {
             if (buf.contains(".")) {
                 String[] input = buf.split("\\.");
+                try {
+                    char[] inspector = input[0].toCharArray();
+                    for (int i = 0; i < inspector.length; i++) {
+                        Character.getNumericValue(inspector[i]);
+                    }
+                    inspector = input[1].toCharArray();
+                    for (int i = 0; i < inspector.length; i++) {
+                        Character.getNumericValue(inspector[i]);
+                    }
+                }
+                catch (Exception ex) {
+                    System.out.println("error! Not numeric to be converted");
+                    return;
+                }
                 int intPart = Integer.parseInt(input[0], sourceBase);
                 char[] decimals = input[1].toCharArray();
                 double fracPart = 0.0;
@@ -34,8 +55,16 @@ public class Main {
                     fracPart = Double.parseDouble("0" + temp.substring(indexOfDecimal));
                 }
             } else {
-                int intPart = Integer.parseInt(buf, sourceBase);
-                int newBase = scanner.nextInt();
+                int intPart;
+                int newBase;
+                try {
+                    intPart = Integer.parseInt(buf, sourceBase);
+                    newBase = scanner.nextInt();
+                }
+                catch (Exception ex) {
+                    System.out.println("error! wrong integer!");
+                    return;
+                }
                 output = getOutput(output, intPart, newBase);
             }
         }
@@ -43,12 +72,15 @@ public class Main {
     }
 
     private static StringBuilder getOutput(StringBuilder output, int intPart, int newBase) {
-        if (newBase > 1) {
+        if (newBase > 1 && newBase < 37) {
             output = new StringBuilder((Integer.toString(intPart, newBase)));
-        } else {
+        } else if (newBase == 1) {
             while (intPart-- > 0) {
                 output.append(1);
             }
+        }
+        else {
+            System.out.println("error! new base is less than 1 or greater than 37");
         }
         return output;
     }
