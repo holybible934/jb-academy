a00d65c7b39f274166c76f934fe528fb2b23e097
CHChang <holybible934@gmail.com>
2021-01-30 23:31:48 +0800

    Refactor Phone Book
    1. Use Instant and Duration
    2. Correct main flow

--- PhoneBook.java
+++ PhoneBook.java
@@ -2,6 +2,8 @@ package phonebook;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.time.Duration;
+import java.time.Instant;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
@@ -9,30 +11,96 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         File find = new File("/Users/chchang/Downloads/find.txt");
-        Scanner scanner;
+        File directory = new File("/Users/chchang/Downloads/directory.txt");
+        Scanner findScanner;
+        Scanner directoryScanner;
         try {
-            scanner = new Scanner(find);
+            findScanner = new Scanner(find);
+            directoryScanner = new Scanner(directory);
         } catch (FileNotFoundException e) {
             e.printStackTrace();
             return;
         }
         List<String> namesInFind = new ArrayList<>();
-        while (scanner.hasNextLine()) {
-            namesInFind.add(scanner.nextLine());
+        List<String> namesInDir = new ArrayList<>();
+        while (findScanner.hasNextLine()) {
+            namesInFind.add(findScanner.nextLine());
         }
-        System.out.println("Start searching...");
-        long beforeSearch = System.currentTimeMillis();
-        for (int i = 0; i < 1000; i++) {
-            for (String str : namesInFind) {
-                //System.out.println(str);
-                StringBuilder sb = new StringBuilder(str);
+        while (directoryScanner.hasNextLine()) {
+            namesInDir.add(directoryScanner.nextLine());
+        }
+        System.out.println("Start linear searching...");
+        Instant linearSearchStart = Instant.now();
+        int counter = doLinearSearch(namesInFind, namesInDir);
+        Instant linearSearchEnd = Instant.now();
+        printLinearSearch(linearSearchStart, linearSearchEnd);
+
+        System.out.println("Start searching(bubble sort + jump search)...");
+        List<String> sortedNamesInDir = new ArrayList<>(namesInDir);
+        Instant bubbleSortStart = Instant.now();
+        bubbleSort(sortedNamesInDir);
+        Instant bubbleSortEnd = Instant.now();
+        if ((Duration.between(linearSearchStart, linearSearchEnd).compareTo(Duration.between(bubbleSortStart, bubbleSortEnd)
+                .dividedBy(10)) < 0)) {
+            linearSearchStart = Instant.now();
+            counter = doLinearSearch(namesInFind, namesInDir);
+            linearSearchEnd = Instant.now();
+            printLinearSearch(bubbleSortStart, bubbleSortStart, linearSearchStart, linearSearchEnd);
+        }
+        else {
+            Instant jumpSearchStart = Instant.now();
+            //counter = doJumpSearch(sortedNamesInDir, namesInFind);
+            Instant jumpSearchEnd = Instant.now();
+            //printJumpSearch(bubbleSortStart, bubbleSortEnd, jumpSearchStart, jumpSearchEnd);
+        }
+    }
+
+    private static void printLinearSearch(Instant bubbleSortStart, Instant bubbleSortEnd, Instant linearSearchStart, Instant linearSearchEnd) {
+        Duration total = Duration.between(bubbleSortStart, linearSearchEnd);
+        long totalMin = total.toMinutes();
+        long totalSec = total.toSeconds();
+        long totalMs = total.toMillisPart();
+        System.out.printf("Found 500 / 500 entries. Time taken: %d min. %d sec. %d ms.", totalMin, totalSec, totalMs);
+        Duration sort = Duration.between(bubbleSortStart,bubbleSortEnd);
+        long sortMin = sort.toMinutes();
+        long sortSec = sort.toSeconds();
+        long sortMS = sort.toMillisPart();
+        System.out.printf("Sorting time: %d min. %d sec. %d ms. - STOPPED, move to linear search", sortMin, sortSec, sortMS);
+        Duration search = Duration.between(linearSearchStart, linearSearchEnd);
+        long searchMin = search.toMinutes();
+        long searchSec = search.toSeconds();
+        long searchMS = search.toMillisPart();
+        System.out.printf("Searching time: %d min. %d sec. %d ms", searchMin, searchSec, searchMS);
+    }
+
+    private static void printLinearSearch(Instant linearSearchStart, Instant linearSearchEnd) {
+        Duration d = Duration.between(linearSearchStart, linearSearchEnd);
+        long minutes = d.toMinutes();
+        long seconds = d.toSeconds();
+        long millis = d.toMillisPart();
+        System.out.printf("Found 500 / 500 entries. Time taken: %d min. %d sec. %d ms.", minutes, seconds, millis);
+    }
+
+    private static int doLinearSearch(List<String> namesInFind, List<String> namesInDir) {
+        int counter = 0;
+        for (String str : namesInFind) {
+            for (String dir : namesInDir) {
+                if (dir.contains(str)) {
+                    counter++;
+                    break;
+                }
             }
         }
-        long afterSearch = System.currentTimeMillis();
-        long minute = (afterSearch - beforeSearch) / 1000 / 60;
-        long second = (afterSearch - beforeSearch) / 1000 % 60;
-        long millisecond = (afterSearch - beforeSearch) % 1000;
-        //System.out.println("Found 500 / 500 entries. Time taken: 1 min. 56 sec. 328 ms.");
-        System.out.printf("Found 500 / 500 entries. Time take: %d min. %d sec. %d ms.%n", minute, second, millisecond);
+        return counter;
+    }
+
+    private static void bubbleSort(List<String> namesInFind) {
+        for (int i = 0; i < namesInFind.size() - 1; i++) {
+            if (namesInFind.get(i).compareTo(namesInFind.get(i + 1)) < 0) {
+                String temp = namesInFind.get(i + 1);
+                namesInFind.remove(i + 1);
+                namesInFind.add(i, temp);
+            }
+        }
     }
 }
