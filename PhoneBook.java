69a3b55c509f2adcced60994227849ef3dbc3a12
CHChang <holybible934@gmail.com>
2021-02-02 10:50:32 +0800

    Add quick sort and binary search

--- PhoneBook.java
+++ PhoneBook.java
@@ -11,7 +11,6 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         File find = new File("/Users/chchang/Downloads/find.txt");
-        File directory = new File("/Users/chchang/Downloads/directory.txt");
         Scanner findScanner;
         Scanner directoryScanner;
         try {
@@ -53,6 +52,82 @@ public class Main {
             Instant jumpSearchEnd = Instant.now();
             printJumpSearch(bubbleSortStart, bubbleSortEnd, jumpSearchStart, jumpSearchEnd);
         }
+
+        System.out.println("Start searching(quick sort + binary search)...");
+        sortedNamesInDir = new ArrayList<>(namesInDir);
+        Instant quickSortStart = Instant.now();
+        doQuickSort(sortedNamesInDir);
+        Instant quickSortEnd = Instant.now();
+        Instant binarySearchStart = Instant.now();
+        counter = doBinarySearch(sortedNamesInDir, namesInFind);
+        Instant binarySearchEnd = Instant.now();
+        printBinarySearch(quickSortStart, quickSortEnd, binarySearchStart, binarySearchEnd);
+    }
+
+    private static void printBinarySearch(Instant quickSortStart, Instant quickSortEnd, Instant binarySearchStart, Instant binarySearchEnd) {
+        Duration total = Duration.between(quickSortStart, binarySearchEnd);
+        long totalMin = total.toMinutesPart();
+        long totalSec = total.toSecondsPart();
+        long totalMs = total.toMillisPart();
+        System.out.printf("Found 500 / 500 entries. Time taken: %d min. %d sec. %d ms.%n", totalMin, totalSec, totalMs);
+        Duration sort = Duration.between(quickSortStart,quickSortEnd);
+        long sortMin = sort.toMinutesPart();
+        long sortSec = sort.toSecondsPart();
+        long sortMS = sort.toMillisPart();
+        System.out.printf("Sorting time: %d min. %d sec. %d ms.%n", sortMin, sortSec, sortMS);
+        Duration search = Duration.between(binarySearchStart, binarySearchEnd);
+        long searchMin = search.toMinutesPart();
+        long searchSec = search.toSecondsPart();
+        long searchMS = search.toMillisPart();
+        System.out.printf("Searching time: %d min. %d sec. %d ms%n", searchMin, searchSec, searchMS);
+    }
+
+    private static int doBinarySearch(List<String> sortedNamesInDir, List<String> namesInFind) {
+        int counter = 0;
+        for (String str : namesInFind) {
+            if (Collections.binarySearch(sortedNamesInDir, str) >= 0) {
+                counter++;
+            }
+        }
+        return counter;
+    }
+
+    private static void doQuickSort(List<String> sortedNamesInDir) {
+        quickSort(sortedNamesInDir, 0, sortedNamesInDir.size() - 1);
+    }
+
+    private static void quickSort(List<String> sortedNamesInDir, int left, int right) {
+        int pivot;
+        if (right > left) {
+            pivot = partition(sortedNamesInDir, left, right);
+            quickSort(sortedNamesInDir, left, pivot - 1);
+            quickSort(sortedNamesInDir, pivot + 1, right);
+        }
+    }
+
+    private static int partition(List<String> sortedNamesInDir, int left, int right) {
+        String P = sortedNamesInDir.get(left);
+        int i = left;
+        int j = right + 1;
+        for (;;) {
+            while (sortedNamesInDir.get(++i).compareTo(P) > 0)
+                if (i >= right)
+                    break;
+            while (sortedNamesInDir.get(--j).compareTo(P) < 0)
+                if (j <= left)
+                    break;
+            if (i >= j) {
+                break;
+            }
+            else {
+                Collections.swap(sortedNamesInDir, i, j);
+            }
+        }
+        if (j == left) {
+            return j;
+        }
+        Collections.swap(sortedNamesInDir, left, j);
+        return j;
     }
 
     private static void printJumpSearch(Instant bubbleSortStart, Instant bubbleSortEnd, Instant jumpSearchStart, Instant jumpSearchEnd) {
@@ -61,7 +136,7 @@ public class Main {
         long totalSec = total.toSecondsPart();
         long totalMs = total.toMillisPart();
         System.out.printf("Found 500 / 500 entries. Time taken: %d min. %d sec. %d ms.%n", totalMin, totalSec, totalMs);
-        Duration sort = Duration.between(bubbleSortStart,bubbleSortEnd);
+        Duration sort = Duration.between(bubbleSortStart, bubbleSortEnd);
         long sortMin = sort.toMinutesPart();
         long sortSec = sort.toSecondsPart();
         long sortMS = sort.toMillisPart();
@@ -102,7 +177,7 @@ public class Main {
         long totalSec = total.toSecondsPart();
         long totalMs = total.toMillisPart();
         System.out.printf("Found 500 / 500 entries. Time taken: %d min. %d sec. %d ms.%n", totalMin, totalSec, totalMs);
-        Duration sort = Duration.between(bubbleSortStart,bubbleSortEnd);
+        Duration sort = Duration.between(bubbleSortStart, bubbleSortEnd);
         long sortMin = sort.toMinutesPart();
         long sortSec = sort.toSecondsPart();
         long sortMS = sort.toMillisPart();
@@ -128,7 +203,6 @@ public class Main {
             for (String dir : namesInDir) {
                 if (dir.contains(str)) {
                     counter++;
-                    break;
                 }
             }
         }
