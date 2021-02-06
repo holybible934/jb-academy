db97afbd155bb54c4a55ec31d89cfc7f41c8a3f6
CHChang <holybible934@gmail.com>
2021-02-06 22:19:38 +0800

    Music Advisor #2

--- MusicAdvisor.java
+++ MusicAdvisor.java
@@ -7,28 +7,44 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String cmd = "";
+        String accessToken = null;
         while (scanner.hasNext()) {
             cmd = scanner.next();
-            switch (cmd) {
-                case "new":
-                    printNewRelease();
-                    break;
-                case "featured":
-                    printFeatured();
-                    break;
-                case "categories":
-                    printCategories();
-                    break;
-                case "playlists":
-                    printPlaylists(scanner);
-                    break;
-                case "exit":
-                    System.out.println("---GOODBYE!---");
-                    return;
-                default:
-                    System.out.println("Not a correct command. Input again!");
+            if (cmd.equals("exit")) {
+                System.out.println("---GOODBYE!---");
+                return;
+            } else if (accessToken == null) {
+                if (cmd.equals("auth")) {
+                    accessToken = doOAuth();
+                    System.out.println("---SUCCESS---");
+                } else {
+                    System.out.println("Please, provide access for application.");
+                }
+            } else {
+                switch (cmd) {
+                    case "new":
+                        printNewRelease();
+                        break;
+                    case "featured":
+                        printFeatured();
+                        break;
+                    case "categories":
+                        printCategories();
+                        break;
+                    case "playlists":
+                        printPlaylists(scanner);
+                        break;
+                    default:
+                        System.out.println("Not a correct command. Input again!");
+                }
             }
         }
+    }
+
+    private static String doOAuth() {
+        String spotifyAuthUrl = "https://accounts.spotify.com/authorize?client_id=0e90298f01c545f3a4d7c47f15ce0a13&redirect_uri=http://localhost:8080&response_type=code";
+        System.out.println(spotifyAuthUrl);
+        return "VALID_ACCESS_TOKEN";
     }
 
     private static void printPlaylists(Scanner scanner) {
