2ec116790446c233e3d04ecb455802045576edcc
CHChang <holybible934@gmail.com>
2021-02-14 16:15:19 +0800

    Complete getNew()

--- Music Advisor/Resources.java
+++ Music Advisor/Resources.java
@@ -1,14 +1,15 @@
 package advisor;
 
-import com.google.gson.JsonObject;
-import com.google.gson.JsonParser;
+import com.google.gson.*;
 
 import java.io.IOException;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 public class Resources {
     private static String API_SERVER_PATH = "https://api.spotify.com";
@@ -24,24 +25,36 @@ public class Resources {
 //        API_SERVER_PATH = "http://127.0.0.1:8080";
     }
 
-    public String[] getNew() {
+    public List<String> getNew() {
         HttpClient client = HttpClient.newBuilder().build();
         HttpRequest request = HttpRequest.newBuilder()
                 .header("Authorization", "Bearer " + accessToken)
-                .header("limit", "5")
                 .uri(URI.create(API_SERVER_PATH + "/v1/browse/new-releases"))
                 .GET()
                 .build();
-        System.out.println(request.headers().firstValue("Authorization").get());
         HttpResponse<String> response = null;
+        List<String> result = new ArrayList<>();
         try {
             response = client.send(request, HttpResponse.BodyHandlers.ofString());
         } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
         if (response != null) {
-            JsonObject jo = JsonParser.parseString(response.body()).getAsJsonObject();
+            JsonArray items = JsonParser.parseString(response.body()).getAsJsonObject().getAsJsonObject("albums").getAsJsonArray("items");
+            for (int i = 0; i < items.size(); i++) {
+                JsonObject item = items.get(i).getAsJsonObject();
+                String albumName = item.get("name").getAsString() + "\n";
+                StringBuilder artistNames = new StringBuilder();
+                JsonArray artists = item.getAsJsonArray("artists");
+//                artistNames.append(artists.getAsJsonObject().get("name").getAsString());
+                for (JsonElement artist : artists) {
+                    artistNames.append(artist.getAsJsonObject().get("name").getAsString());
+                }
+                artistNames.append("\n");
+                String externalUrls = item.getAsJsonObject("external_urls").get("spotify").getAsString();
+                result.add((albumName + artistNames + externalUrls));
+            }
         }
-        return null;
+        return result;
     }
 }
