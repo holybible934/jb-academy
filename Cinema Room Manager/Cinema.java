402a53a630b9c46ff075ef5ad0f0121f0d444bd0
CHChang <holybible934@gmail.com>
2021-02-15 17:04:17 +0800

    Refactor with char[][] and complete assignment #4

--- Cinema Room Manager/Cinema.java
+++ Cinema Room Manager/Cinema.java
@@ -8,56 +8,49 @@ public class Cinema {
         // Write your code here
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter the number of rows:");
-        final int rows = scanner.nextInt();
+        final int rows = scanner.nextInt() + 1;
         System.out.println("Enter the number of seats in each row:");
-        final int seatsPerRow = scanner.nextInt();
-        printSeats(rows, seatsPerRow);
+        final int seatsPerRow = scanner.nextInt() + 1;
+        char[][] cinemaRoom = setupCinema(rows, seatsPerRow);
+        printMenu();
+        while (scanner.hasNext()) {
+            int cmd = scanner.nextInt();
+            switch (cmd) {
+                case 1:
+                    printCinemaRoom(cinemaRoom);
+                    break;
+                case 2:
+                    buyATicket(scanner, cinemaRoom);
+                    break;
+                case 0:
+                    return;
+            }
+            printMenu();
+        }
+    }
+
+    private static void buyATicket(Scanner scanner, char[][] cinemaRoom) {
         System.out.println("Enter a row number:");
         final int seatRow = scanner.nextInt();
         System.out.println("Enter a seat number in that row:");
         final int seatColumn = scanner.nextInt();
-        int ticketPrice = checkTicketPrice(rows, seatsPerRow, seatRow, seatColumn);
+        cinemaRoom[seatRow][seatColumn] = 'B';
+        int ticketPrice = checkTicketPrice(cinemaRoom.length - 1, cinemaRoom[0].length - 1, seatRow, seatColumn);
         System.out.println("Ticket price: $" + ticketPrice);
-        printBookedSeat(rows, seatsPerRow, seatRow, seatColumn);
     }
 
-    private static void printBookedSeat(int rows, int seatsPerRow, int seatRow, int seatColumn) {
-        System.out.println("Cinema:");
-        printRowHeader(seatsPerRow);
-        System.out.println(seatsPerRow);
-        for (int i = 1; i <= rows; i++) {
-            System.out.print(i + " ");
-            if (i != seatRow) {
-                for (int j = 1; j < seatsPerRow; j++) {
-                    System.out.print("S ");
-                }
-                System.out.println("S");
-            } else {
-                if (seatColumn != seatsPerRow) {
-                    for (int j = 1; j < seatsPerRow; j++) {
-                        if (j == seatColumn) {
-                            System.out.print("B ");
-                        } else {
-                            System.out.print("S ");
-                        }
-                    }
-                    System.out.println("S");
-                } else {
-                    for (int j = 1; j < seatsPerRow; j++) {
-                        System.out.print("S ");
-                    }
-                    System.out.println("B");
-                }
-            }
-        }
+    private static void printMenu() {
+        System.out.println("1. Show the seats\n" +
+                "2. Buy a ticket\n" +
+                "0. Exit");
     }
 
-    private static void printRowHeader(int seatsPerRow) {
-        for (int i = 0; i < seatsPerRow; i++) {
+    private static void initRowHeader(char[][] cinemaRoom) {
+        for (int i = 0; i < cinemaRoom[0].length; i++) {
             if (i == 0) {
-                System.out.print("  ");
+                cinemaRoom[0][i] = ' ';
             } else {
-                System.out.print(i + " ");
+                cinemaRoom[0][i] = (char) (i + '0');
             }
         }
     }
@@ -74,16 +67,28 @@ public class Cinema {
         }
     }
 
-    private static void printSeats(int rows, int seatsPerRow) {
-        System.out.println("Cinema:");
-        printRowHeader(seatsPerRow);
-        System.out.println(seatsPerRow);
-        for (int i = 1; i <= rows; i++) {
-            System.out.print(i + " ");
+    private static char[][] setupCinema(int rows, int seatsPerRow) {
+        char[][] cinemaRoom = new char[rows][seatsPerRow];
+        initRowHeader(cinemaRoom);
+        for (int i = 1; i < rows; i++) {
+            cinemaRoom[i][0] = (char) (i + '0');
             for (int j = 1; j < seatsPerRow; j++) {
-                System.out.print("S ");
+                cinemaRoom[i][j] = 'S';
             }
-            System.out.println("S");
         }
+        printCinemaRoom(cinemaRoom);
+        return cinemaRoom;
+    }
+
+    private static void printCinemaRoom(char[][] cinemaRoom) {
+        System.out.println("Cinema:");
+        for (char[] rows : cinemaRoom) {
+            for (char seat : rows) {
+                System.out.print(seat);
+                System.out.print(' ');
+            }
+            System.out.println();
+        }
+        System.out.println();
     }
 }
