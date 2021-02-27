98505265a5e57ae931ea45956904aee73b8a0241
CHChang <holybible934@gmail.com>
2021-02-27 22:31:46 +0800

    Code Sharing Platform
    Spring Boot sample

---
+++ Code Sharing Platform/Code Sharing Platform/task/src/platform/CodeSharingPlatform.java
@@ -0,0 +1,39 @@
+package platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletResponse;
+
+@SpringBootApplication
+@RestController
+public class CodeSharingPlatform {
+
+    private Code code = new Code("System.out.println(\"Hello World\");");
+
+    public static void main(String[] args) {
+        SpringApplication.run(CodeSharingPlatform.class, args);
+    }
+
+    @GetMapping(value = "/code")
+    public ModelAndView getHtml(HttpServletResponse response) {
+        response.addHeader("Content-Type", "text/html");
+        String codeBody = code.getCode();
+
+        ModelAndView model = new ModelAndView("codePage");
+        model.addObject("codeBody", codeBody);
+        return model;
+
+    }
+
+    @GetMapping(value = "/api/code")
+    public Code getJson(HttpServletResponse response) {
+        response.addHeader("Content-Type", "application/json");
+
+        return code;
+    }
+
+}
