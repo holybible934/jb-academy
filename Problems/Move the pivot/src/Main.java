53ccc0b52d47928d3c1c09beaab863d6120fdb87
CHChang <holybible934@gmail.com>
2021-03-21 21:01:53 +0800

    Add arbitrary chosen element as the 1st pivot for Quick Sort

---
+++ Problems/Move the pivot/src/Main.java
@@ -0,0 +1,48 @@
+import java.util.*;
+
+public class Main {
+
+    public static void moveThePivot(int[] array, int pivotIndex) {
+        // write your code here
+        // Move chosen pivot value to the end of the array
+        int swapTemp = array[array.length - 1];
+        array[array.length - 1] = array[pivotIndex];
+        array[pivotIndex] = swapTemp;
+        quickSort(array, 0, array.length - 1);
+    }
+
+    private static void quickSort(int[] array, int begin, int end) {
+        if (begin < end) {
+            int partitionIndex = partition(array, begin, end);
+            quickSort(array, begin, partitionIndex - 1);
+            quickSort(array, partitionIndex + 1, end);
+        }
+    }
+
+    private static int partition(int[] array, int begin, int end) {
+        int pivot = array[end];
+        int i = begin - 1;
+        for (int j = begin; j < end; j++) {
+            if (array[j] <= pivot) {
+                i++;
+                int swapTemp = array[i];
+                array[i] = array[j];
+                array[j] = swapTemp;
+            }
+        }
+        int swapTemp = array[i + 1];
+        array[i + 1] = array[end];
+        array[end] = swapTemp;
+        return i + 1;
+    }
+
+    /* Do not change code below */
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int[] array = Arrays.stream(scanner.nextLine().split(" "))
+                .mapToInt(Integer::parseInt).toArray();
+        int pivotIndex = scanner.nextInt();
+        moveThePivot(array, pivotIndex);
+        Arrays.stream(array).forEach(e -> System.out.print(e + " "));
+    }
+}
