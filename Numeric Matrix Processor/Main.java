12afe56121a2879057394b7089810559d1e817b4
CHChang <holybible934@gmail.com>
2021-04-05 22:25:23 +0800

    Assignment#3 - pass test#4
    1. Refactor with a console flow
    2. Support double
    3. Refactor doMultiplyConstant and doMatrixAddition

--- Numeric Matrix Processor/Main.java
+++ Numeric Matrix Processor/Main.java
@@ -6,6 +6,34 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        printMenu();
+        System.out.print("Your choice: ");
+        int choice = Integer.parseInt(scanner.nextLine());
+        while (choice != 0) {
+            switch (choice) {
+                case 1:
+                    doMatrixAddition(scanner);
+                    break;
+                case 2:
+                    doMultiplyConstant(scanner);
+                    break;
+                case 3:
+                    doMultiplyByMatrices(scanner);
+                    break;
+                default:
+                    break;
+            }
+            printMenu();
+            System.out.print("Your choice: ");
+            try {
+                choice = Integer.parseInt(scanner.nextLine());
+            } catch (NumberFormatException e) {
+                System.out.println("Wrong Input");
+                choice = -1;
+            }
+        }
+    }
+
         int rowOfMatrixA = scanner.nextInt();
         int columnOfMatrixA = scanner.nextInt();
         scanner.nextLine();
@@ -14,35 +42,63 @@ public class Main {
             String[] input = scanner.nextLine().split(" ");
             matrixA[i] = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
         }
-//        doMaxtrixAddition(scanner, rowOfMatrixA, columnOfMatrixA, matrixA);
-        doMultiplyConstant(scanner, rowOfMatrixA, matrixA);
     }
 
-    private static void doMultiplyConstant(Scanner scanner, int rowOfMatrixA, int[][] matrixA) {
-        int cons = scanner.nextInt();
+    private static void printMenu() {
+        System.out.println("1. Add matrices\n" +
+                "2. Multiply matrix by a constant\n" +
+                "3. Multiply matrices\n" +
+                "0. Exit");
+    }
+
+    private static void doMultiplyConstant(Scanner scanner) {
+        int rowOfMatrixA = scanner.nextInt();
+        int columnOfMatrixA = scanner.nextInt();
+        scanner.nextLine();
+        double[][] matrixA = new double[rowOfMatrixA][columnOfMatrixA];
         for (int i = 0; i< rowOfMatrixA; i++) {
-            matrixA[i] = Arrays.stream(matrixA[i])
-                    .map(e -> e * cons)
-                    .toArray();
+            String[] input = scanner.nextLine().split(" ");
+            matrixA[i] = Arrays.stream(input).mapToDouble(Double::parseDouble).toArray();
         }
+        double cons = Double.parseDouble(scanner.nextLine());
+        System.out.println("The result is:");
+//        for (int i = 0; i< rowOfMatrixA; i++) {
+//            matrixA[i] = Arrays.stream(matrixA[i])
+//                    .map(e -> e * cons)
+//                    .toArray();
+//        }
         for (int i = 0; i< rowOfMatrixA; i++) {
-            Arrays.stream(matrixA[i]).forEach(e -> System.out.print(e + " "));
+            Arrays.stream(matrixA[i]).forEach(e -> System.out.print(e * cons + " "));
             System.out.println();
         }
+        System.out.println();
     }
 
-    private static void doMaxtrixAddition(Scanner scanner, int rowOfMatrixA, int columnOfMatrixA, int[][] matrixA) {
+    private static void doMatrixAddition(Scanner scanner) {
+        System.out.print("Enter size of first matrix: ");
+        int rowOfMatrixA = scanner.nextInt();
+        int columnOfMatrixA = scanner.nextInt();
+        scanner.nextLine();
+        double[][] matrixA = new double[rowOfMatrixA][columnOfMatrixA];
+        System.out.println("Enter first matrix:");
+        for (int i = 0; i< rowOfMatrixA; i++) {
+            String[] input = scanner.nextLine().split(" ");
+            matrixA[i] = Arrays.stream(input).mapToDouble(Double::parseDouble).toArray();
+        }
+        System.out.print("Enter size of second matrix: ");
         int rowOfMatrixB = scanner.nextInt();
         int columnOfMatrixB = scanner.nextInt();
         scanner.nextLine();
-        int[][] matrixB = new int[rowOfMatrixB][columnOfMatrixB];
+        double[][] matrixB = new double[rowOfMatrixB][columnOfMatrixB];
+        System.out.println("Enter second matrix:");
         for (int i = 0; i< rowOfMatrixB; i++) {
             String[] input = scanner.nextLine().split(" ");
-            matrixB[i] = Arrays.stream(input).mapToInt(Integer::parseInt).toArray();
+            matrixB[i] = Arrays.stream(input).mapToDouble(Double::parseDouble).toArray();
         }
         if (rowOfMatrixA != rowOfMatrixB || columnOfMatrixA != columnOfMatrixB) {
-            System.out.println("ERROR");
+            System.out.println("The operation cannot be performed.");
         } else {
+            System.out.println("The result is:");
             for (int i = 0; i < rowOfMatrixA; i++) {
                 for (int j = 0; j < columnOfMatrixA; j++) {
                     System.out.print(matrixA[i][j] + matrixB[i][j]);
