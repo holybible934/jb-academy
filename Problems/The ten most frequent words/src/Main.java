791f13cf0b3c52fbada795ad7e99a5c51e35f2bc
CHChang <holybible934@gmail.com>
2021-04-17 17:46:16 +0800

    Refactor
    1. Use \\W+ to split inputline
    2. Remove comment

--- Problems/The ten most frequent words/src/Main.java
+++ Problems/The ten most frequent words/src/Main.java
@@ -1,16 +1,14 @@
-import java.text.Normalizer;
-import java.util.*;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine().toLowerCase();
-        String withoutAccent = Normalizer.normalize(input, Normalizer.Form.NFD);
-        String output = withoutAccent.replaceAll("[^a-zA-Z0-9 ]", "");
 
-        List<String> inputList = Arrays.asList(output.split("\\s+"));
+        List<String> inputList = Arrays.asList(scanner.nextLine().toLowerCase().split("\\W+"));
         List<Word> wordsList = inputList.stream().distinct().map(Word::new).collect(Collectors.toList());
         for (Word w : wordsList) {
             w.setCount(inputList.stream()
@@ -18,7 +16,6 @@ public class Main {
                     .count());
         }
         List<Word> sortedTopTenList = wordsList.stream()
-//                .sorted((word, t1) -> (int) (t1.getCount() - word.getCount()))
                 .sorted((word, t1) -> {
                     if (word.getCount() != t1.getCount()) {
                         return (int) (t1.getCount() - word.getCount());
