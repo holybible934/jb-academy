33af319b4a2c415f814ba72ac17acd6d9dbb012f
CHChang <holybible934@gmail.com>
2021-04-17 17:52:08 +0800

    Refactor the ten most frequent words
    By using stream API only

--- Problems/The ten most frequent words/src/Main.java
+++ Problems/The ten most frequent words/src/Main.java
@@ -1,53 +1,20 @@
-import java.util.Arrays;
-import java.util.List;
 import java.util.Scanner;
+import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 public class Main {
     public static void main(String[] args) {
         // write your code here
-        Scanner scanner = new Scanner(System.in);
-
-        List<String> inputList = Arrays.asList(scanner.nextLine().toLowerCase().split("\\W+"));
-        List<Word> wordsList = inputList.stream().distinct().map(Word::new).collect(Collectors.toList());
-        for (Word w : wordsList) {
-            w.setCount(inputList.stream()
-                    .filter(str -> str.compareTo(w.getContent()) == 0)
-                    .count());
-        }
-        List<Word> sortedTopTenList = wordsList.stream()
-                .sorted((word, t1) -> {
-                    if (word.getCount() != t1.getCount()) {
-                        return (int) (t1.getCount() - word.getCount());
-                    } else {
-                        return word.getContent().compareTo(t1.getContent());
-                    }
+        Stream.of(new Scanner(System.in).nextLine().split("\\W+"))
+                .map(String::toLowerCase)
+                .collect(Collectors.groupingBy(Function.identity(), Collectors.counting()))
+                .entrySet().stream()
+                .sorted((o1, o2) -> {
+                    final long valueDiff = o2.getValue() - o1.getValue();
+                    return valueDiff == 0 ? o1.getKey().compareTo(o2.getKey()) : (int) valueDiff;
                 })
                 .limit(10)
-                .collect(Collectors.toList());
-        sortedTopTenList.stream().map(Word::getContent).forEach(System.out::println);
-    }
-}
-
-class Word {
-
-    private final String content;
-    private long count;
-
-    public long getCount() {
-        return count;
-    }
-
-    public void setCount(long count) {
-        this.count = count;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public Word(String word) {
-        this.content = word;
-        this.count = 1;
+                .forEach(e -> System.out.println(e.getKey()));
     }
 }
