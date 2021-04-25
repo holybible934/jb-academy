02e3e70e40b491b96b39b542fd5b8b5d1e78b8f0
CHChang <holybible934@gmail.com>
2021-04-25 21:31:27 +0800

    Add Company and Car classes

---
+++ Car Sharing/Car Sharing/task/src/carsharing/Car.java
@@ -0,0 +1,39 @@
+package carsharing;
+
+public class Car {
+    private int id;
+    private int company_id;
+    private String name;
+
+    public Car(int id, int company_id, String name) {
+        this.id = id;
+        this.company_id = company_id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getCompany_id() {
+        return company_id;
+    }
+
+    public void setCompany_id(int company_id) {
+        this.company_id = company_id;
+    }
+
+
+}
