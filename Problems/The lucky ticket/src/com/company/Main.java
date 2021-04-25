1ebd210d7d147399f0dc31c4efba35fe4f5cb5bc
CHChang <holybible934@gmail.com>
2021-04-25 20:34:33 +0800

    Refactor - Use IntStream

--- Problems/The lucky ticket/src/com/company/Main.java
+++ Problems/The lucky ticket/src/com/company/Main.java
@@ -5,16 +5,11 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    // write your code here
         Scanner scanner = new Scanner(System.in);
-        String ticketNum = scanner.nextLine();
-        final int ticketLength = 6;
-        int sumOfFirst = 0;
-        int sumOfLast = 0;
-        for (int i = 0; i < ticketLength / 2; i++) {
-            sumOfFirst += Integer.parseInt(ticketNum.substring(i, i + 1));
-            sumOfLast += Integer.parseInt(ticketNum.substring(i + ticketLength / 2, i + ticketLength / 2 + 1));
-        }
+        String ticket = scanner.nextLine();
+        int sumOfFirst = ticket.substring(0, ticket.length() / 2).chars().sum();
+        int sumOfLast = ticket.substring(ticket.length() / 2).chars().sum();
         System.out.println(sumOfFirst == sumOfLast ? "Lucky" : "Regular");
     }
 }
